931f9ac08f546e1b8092bdf74bfa1d92a03b9040
corey <2278551479@qq.com>
2018-03-20 21:55:23 +0800

    change the file exponential.cpp

--- exponential/main.cpp
+++ exponential/main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-	printf("Ni hao%s!",word);
+    int origin[50],multi[50],result[50];
+    int i=0;
+    for(;i<50;i++)
+    {
+        origin[i]=0;
+        multi[i]=0;
+        result[i]=0;
+    }
+    int or_num = 0, mul_num = 0;
+
+	printf("Ni hao%s!","word");
     return 0;
 }
